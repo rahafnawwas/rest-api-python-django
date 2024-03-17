d7560fbffc66f2b79533e6b999a932253ae95315
rahaf nawwas <rahafnawas6@gmail.com>
2024-03-17 02:30:04 +0200

    create get api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class HelloApiView(APIView):
+    """Test API view"""
+
+    def get(self,request,format=None):
+        """returns a list of apiview"""
+        an_apiview=[
+        'uses http methods as function',
+        'is similar to a trafitional',
+        'is mapped manually to URLs',
+        ]
+
+        return Response({'message':'hello world!','an_apiview':an_apiview})
